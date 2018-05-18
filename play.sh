273a7b8aa3d719d0b8700bc2206759c7f0cfe6fd
ryanzl <killerggf@qq.com>
2018-05-18 17:22:08 +0800

    修改错误

--- play.sh
+++ play.sh
@@ -3,23 +3,23 @@ cp apache-rocketmq.tar.gz ./namesrv && cp apache-rocketmq.tar.gz ./broker && cp 
 
 docker build -t apache/rocketmq-namesrv:4.1.0-incubating --build-arg "ROCKETMQ_VERSION=4.1.0-incubating" ./namesrv
 
-docker build -t apache/rocketmq-broker:4.1.0-incubating \ 
- --build-arg "ROCKETMQ_VERSION=4.1.0-incubating" \ 
- --build-arg "BROKER_IP1=192.168.59.203" \ 
- --build-arg "BROKER_IP2=192.168.59.203" \ 
+docker build -t apache/rocketmq-broker:4.1.0-incubating \
+ --build-arg "ROCKETMQ_VERSION=4.1.0-incubating" \
+ --build-arg "BROKER_IP1=127.0.0.1" \
+ --build-arg "BROKER_IP2=127.0.0.1" \
  ./broker
 
 docker build -t apache/rocketmq-console:1.0.0 ./console
 
 docker run -d -p 9876:9876 --name rmqnamesrv --restart=always apache/rocketmq-namesrv:4.1.0-incubating
 
-docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --restart=always -m 2048m \ 
- -e "NAMESRV_ADDR=rmqnamesrv:9876" \ 
- -e "JAVA_OPT=-XX:MetaspaceSize=256m" \ 
- -v /opt/rocketmq/logs:/opt/logs \ 
- -v /opt/rocketmq/store:/opt/store \ 
+docker run -d -p 10911:10911 -p 10909:10909 --name rmqbroker --restart=always -m 2048m \
+ -e "NAMESRV_ADDR=rmqnamesrv:9876" \
+ -e "JAVA_OPT=-XX:MetaspaceSize=256m" \
+ -v /Users/ryanzl/docker-data/rocketmq/logs:/opt/logs \
+ -v /Users/ryanzl/docker-data/rocketmq/store:/opt/store \
  --link rmqnamesrv:rmqnamesrv apache/rocketmq-broker:4.1.0-incubating
 
-docker run -d -p 8888:8080 --name rmqconsole --restart=always -m 512m \ 
+docker run -d -p 8888:8080 --name rmqconsole --restart=always -m 512m \
  -e NAMESRV_ADDR='rmqnamesrv:9876' \
  --link rmqnamesrv:rmqnamesrv apache/rocketmq-console:1.0.0
